d187282707fe7af3f9ce28557ef242e882e2a2ae
Mel <57514727+Mel1207@users.noreply.github.com>
2024-07-15 14:35:02 +0800

    added logo on partners section

--- src/components/PartnersSection.jsx
+++ src/components/PartnersSection.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { partnerList } from '../constants'
 
 const PartnersSection = () => {
   return (
     <div className="partners-section">
       <div className='container'>
         <h2>With over 200+ partnered clients all over the world</h2>
+        <div className='partner-list'>
+          {partnerList.map(item => (
+            <img src={item.path} alt={item.title} key={item.id}/>
+          ))}
+        </div>
       </div>
     </div>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,10 @@
+import Partner1 from '../assets/logo1.svg'
+import Partner2 from '../assets/logo2.svg'
+import Partner3 from '../assets/logo3.svg'
+import Partner4 from '../assets/logo4.svg'
+import Partner5 from '../assets/logo5.svg'
+
+
 export const navLinks = [
   {
     id: 1, 
@@ -18,5 +25,33 @@ export const navLinks = [
     id: 4, 
     title: 'Careers', 
     path: '/'
+  },
+]
+
+export const partnerList = [
+  {
+    id: 1, 
+    title: 'Partner 1',
+    path: Partner1
+  },
+  {
+    id: 2, 
+    title: 'Partner 2',
+    path: Partner2
+  },
+  {
+    id: 3, 
+    title: 'Partner 3',
+    path: Partner3
+  },
+  {
+    id: 4, 
+    title: 'Partner 4',
+    path: Partner4
+  },
+  {
+    id: 5, 
+    title: 'Partner 5',
+    path: Partner5
   },
 ]
