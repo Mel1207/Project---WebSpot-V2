be57cba4bdf7692afdc66ad2843a3228326a85b2
Mel <57514727+Mel1207@users.noreply.github.com>
2024-12-15 21:09:56 +0800

    added cards for testimonial

--- src/components/SectionTestimonial.jsx
+++ src/components/SectionTestimonial.jsx
@@ -4,13 +4,16 @@ const SectionTestimonial = () => {
   return (
     <div className='section-testimonial'>
       <div className="container">
-        <h2>What our client say</h2>
-        <p>ITs simply dummy text of the printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard dummy text </p>
+        <div className="testimonial-text">
+          <h2>What our client say</h2>
+          <p>ITs simply dummy text of the printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard dummy text </p>
+        </div>
+       
 
         <div className='testimonials'>
-          <div>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</div>
-          <div>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</div>
-          <div>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</div>
+          <div className='testimonial-card'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <div className='outliner'></div></div>
+          <div className='testimonial-card'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <div className='outliner'></div></div>
+          <div className='testimonial-card'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <div className='outliner'></div></div>
         </div>
       </div>
       
