ec6124832dd3b0a0b5f10309e97aa25cf24958d7
Mel <57514727+Mel1207@users.noreply.github.com>
2024-12-17 00:32:53 +0800

    added spacing for faq

--- src/components/SectionFAQ.jsx
+++ src/components/SectionFAQ.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const SectionFAQ = () => {
   return (
-    <div className='container'>
-      <div className="testimonial-text">
-        <h2>Frequently Asked Questions</h2>
-        <p>ITs simply dummy text of the printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard dummy text </p>
+    <div className="section-faq">
+      <div className='container'>
+        <div className="testimonial-text">
+          <h2>Frequently Asked Questions</h2>
+          <p>ITs simply dummy text of the printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard dummy text </p>
+        </div>
       </div>
     </div>
+   
   )
 }
 
