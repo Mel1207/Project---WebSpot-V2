911ee62a8cc38735cad3194340a6c5afb540bd3e
Mel <57514727+Mel1207@users.noreply.github.com>
2024-12-23 08:01:26 +0800

    added footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import FooterLogo from '../assets/LOGO.svg'
+import { navLinks } from '../constants'
 
 const Footer = () => {
   return (
     <footer className='footer'>
       <div className="container">
-        <h1>This is footer</h1>
+        <div>
+          <img src={FooterLogo} alt="logo" />
+          {navLinks.map(item => (
+            <a href={item.path}>{item.title}</a>
+          ))}
+          <div>
+            {/* THIS IS LOGOs */}
+          </div>
+        </div>
+        <div>
+          <span>Copyright@webspot2024</span>
+          <div>
+            <a href="#!">Terms</a>
+            <a href="#!">Conditions</a>
+            <a href="#!">Privacy Policy</a>
+          </div>
+        </div>
       </div>
     </footer>
   )
