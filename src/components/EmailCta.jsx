db27a10c168179d5deba7a42c5a87684dfd673be
Mel <57514727+Mel1207@users.noreply.github.com>
2024-12-25 01:31:47 +0800

    added email cta

--- src/components/EmailCta.jsx
+++ src/components/EmailCta.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Button from './Button'
+import EmailCtaFigure from '../assets/email-cta-figure.svg'
 
 const EmailCta = () => {
   return (
     <div className="container">
       <div className='email-cta'>
-        <div className="email-text">
-          <h2>Need a website ?</h2>
-          <p>ITs simply dummy text of the printing and typesetting industry. <br /> LoremIpsum has been the industry's standard dummy text </p>
+        <div>
+          <div className="email-text">
+            <h2>Need a website ?</h2>
+            <p>ITs simply dummy text of the printing and typesetting industry. <br /> LoremIpsum has been the industry's standard dummy text </p>
+          </div>
+          <Button title='Get Quotations' classes='btn btn-red'/>
         </div>
-        <Button title='Get Quotations' classes='btn btn-primary'/>
+        <img src={EmailCtaFigure} alt="" className='email-figure'/>
       </div>
 
     </div>
